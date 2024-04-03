c3e3c31b81e291aaacc1cde28f3ef7ab245a31e8
JuliaCaesar <101206316+JuliaCaesar@users.noreply.github.com>
2024-04-02 19:45:44 -0500

    pt 6

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -25,3 +25,12 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
